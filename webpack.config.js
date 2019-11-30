0ae0ee8d7f8c5094306b99943601441baba27e9a
Josh Mazen <joshmazen14@gmail.com>
2019-11-29 22:05:00 -0500

    Make docs

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     libraryTarget: 'umd',
   },
   watchOptions: {
